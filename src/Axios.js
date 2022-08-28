76b962c891931ac2db49f4c95d65dfc38e879c0c
Nano-Stark <inventorstark7@gmail.com>
2022-08-28 04:25:05 +0100

    comment in axios.js

--- src/Axios.js
+++ src/Axios.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 //  I hosted my server on heroku
-const Axios = axios.create({
-  baseURL: "https://yt-clone2.herokuapp.com/api",
-  withCredentials: true,
-});
-
 // const Axios = axios.create({
-//   baseURL: "http://localhost:8800/api",
+//   baseURL: "https://yt-clone2.herokuapp.com/api",
 //   withCredentials: true,
 // });
+
+//  i already shifted my front end as static folder to nodejs on heroku
+//  i don't require starktube.netlify.app as heroku does not support access token
+//  REBUILD the frontend if their is another hosting server which supports cookies
+//  unlike heroku to the above axios configuration.
+const Axios = axios.create({
+  baseURL: "http://localhost:8800/api",
+  withCredentials: true,
+});
 
 export default Axios;
